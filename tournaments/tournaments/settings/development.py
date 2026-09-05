10a3b6a1a3bd2353e522d59a4810c76ecf8ae496
beny25585 <binyamin25585@gmail.com>
2026-09-05 12:46:43 +0300

    fix tournaments local service config

--- tournaments/tournaments/settings/development.py
+++ tournaments/tournaments/settings/development.py
@@ -14,8 +14,10 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
+    'http://localhost:5175',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
+    'http://127.0.0.1:5175',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'https://*.nip.io',
